022c61b16d3c2b2ef17bba0883ae8bb43d84a8fd
Noah-OConnor <no998698@ucf.edu>
2025-07-12 18:16:31 -0400

    Add world generation config asset and header
    Introduces DA_WorldGenConfig.uasset and WorldGenerationConfig.h to define core terrain and biome generation settings, including voxel and chunk sizes, biome data table, and noise parameters for temperature, habitability, and elevation.

---
+++ Source/Bloxels/Voxel/World/WorldGenerationConfig.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "Biome/NoiseInfo.h"
+#include "WorldGenerationConfig.generated.h"
+
+UCLASS(BlueprintType)
+class BLOXELS_API UWorldGenerationConfig : public UDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    // Core terrain structure
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Voxel|World")
+    int32 VoxelSize = 100;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Voxel|World")
+    int32 ChunkSize = 16;
+
+    // Biomes
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Voxel|Biome|Data")
+    UDataTable* BiomeDataTable;
+
+    // Biome Noise Settings
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Voxel|Biome|Noise Settings")
+    FNoiseInfo Temperature;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Voxel|Biome|Noise Settings")
+    FNoiseInfo Habitability;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Voxel|Biome|Noise Settings")
+    FNoiseInfo Elevation;
+};
